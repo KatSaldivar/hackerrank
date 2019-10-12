19d41d081d008266b0f08d7350e6c0eed6b98de8
Katherine Saldivar <katherinekaufmann1@gmail.com>
2019-10-12 15:26:29 -0700

    repeated strings

--- src/app/App.java
+++ src/app/App.java
@@ -7,7 +7,7 @@ public class App {
         int pairsOfSocks = SockMerchant.sockMerchant(9, new int[] {10, 20, 20, 10, 10, 30, 50, 10, 20});
         int valleys=CountingValleys.countingValleys(8, "UDDDUDUU");
         int clouds = JumpingOnClouds.jumpingOnClouds(new int[] {0,0,1,0,0,1,0});
-        long repeats = RepeatedString.repeatedString("abcac", 10);
+        long repeats = RepeatedString.repeatedString("aba", 10);
         System.out.println(repeats);
 
     }
--- src/app/solutions/RepeatedString.java
+++ src/app/solutions/RepeatedString.java
@@ -12,8 +12,41 @@ public class RepeatedString {
 
     // Complete the repeatedString function below.
     public static long repeatedString(String s, long n) {
-        return 298375827;
+        //count the As in s
+        int aCount = 0;
+        String[] letters = s.split("");
+        for(String letter:letters){
+            if (letter.equals("a")){
+                aCount++;
+            }
+        }
+        //count how many s to reach n and get the remainder 
+        long sRepetitions = n/s.length();
+        long remainder = n%s.length();
 
+        //multiply the counts and determine remainders
+        long count = aCount * sRepetitions;
+        for(int i=0; i<remainder; i++){
+            if(letters[i].equals("a")){
+                count++;
+            }
+        }
+        return count;
+
+       /* //repeat string until length ==n
+        StringBuilder string= new StringBuilder();
+        while(string.length()<n){
+            string.append(s);
+        }
+        //split into arr and count for letter a
+        String[] letters = string.toString().split("");
+        int count = 0;
+        for(String letter:letters){
+            if (letter.equals("a")){
+                count++;
+            }
+        }
+        return count;*/
     }
 
     private static final Scanner scanner = new Scanner(System.in);
